10f0fd3a31581b04e0718e30c8c3f486b8af0e25
courtneymorris <shull.courtney@gmail.com>
2021-12-21 08:17:07 -0700

    Change background of parent on click

--- src/components/child.js
+++ src/components/child.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function child(props) {
-  return (
-    <div className="child">
-      <h2 className="component-header">Child</h2>
-    </div>
-  );
+export default class Child extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  render() {
+    return (
+      <div
+        className="child"
+        style={{ backgroundColor: this.props.handleClick }}
+      >
+        <h2 className="component-header">Child</h2>
+      </div>
+    );
+  }
 }
